c98d8a1d662e05f34f0c0af3d5a76c4a41f97afb
Khem Neupane <shekharneupane@gmail.com>
2023-09-02 18:27:35 +0300

    added buba images

--- src/App.js
+++ src/App.js
@@ -1,185 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
-import Gallary from "./components/gallery";
+import IndiraGallary from "./components/gallery/didi";
+import BubaGallary from "./components/gallery/buba";
 
 function App() {
-  const images = [
-    {
-      original: "/assets/1.jpg",
-      thumbnail: "/assets/1.jpg",
-    },
-    {
-      original: "/assets/2.jpg",
-      thumbnail: "/assets/2.jpg",
-    },
-    {
-      original: "/assets/5.jpg",
-      thumbnail: "/assets/5.jpg",
-    },
-    {
-      original: "/assets/6.jpg",
-      thumbnail: "/assets/6.jpg",
-    },
-    {
-      original: "/assets/7.jpg",
-      thumbnail: "/assets/7.jpg",
-    },
-    {
-      original: "/assets/8.jpg",
-      thumbnail: "/assets/8.jpg",
-    },
-    {
-      original: "/assets/9.jpg",
-      thumbnail: "/assets/9.jpg",
-    },
-    {
-      original: "/assets/10.jpg",
-      thumbnail: "/assets/10.jpg",
-    },
-    {
-      original: "/assets/11.jpg",
-      thumbnail: "/assets/11.jpg",
-    },
-    {
-      original: "/assets/12.jpg",
-      thumbnail: "/assets/12.jpg",
-    },
-    {
-      original: "/assets/13.jpg",
-      thumbnail: "/assets/13.jpg",
-    },
-    {
-      original: "/assets/14.jpg",
-      thumbnail: "/assets/14.jpg",
-    },
-    {
-      original: "/assets/15.jpg",
-      thumbnail: "/assets/15.jpg",
-    },
-    {
-      original: "/assets/16.jpg",
-      thumbnail: "/assets/16.jpg",
-    },
-    {
-      original: "/assets/17.jpg",
-      thumbnail: "/assets/17.jpg",
-    },
-    {
-      original: "/assets/18.jpg",
-      thumbnail: "/assets/18.jpg",
-    },
-    {
-      original: "/assets/19.jpg",
-      thumbnail: "/assets/19.jpg",
-    },
-    {
-      original: "/assets/20.jpg",
-      thumbnail: "/assets/20.jpg",
-    },
-    {
-      original: "/assets/21.jpg",
-      thumbnail: "/assets/21.jpg",
-    },
-    {
-      original: "/assets/22.jpg",
-      thumbnail: "/assets/22.jpg",
-    },
-    {
-      original: "/assets/23.jpg",
-      thumbnail: "/assets/23.jpg",
-    },
-    {
-      original: "/assets/24.jpg",
-      thumbnail: "/assets/24.jpg",
-    },
-    {
-      original: "/assets/25.jpg",
-      thumbnail: "/assets/25.jpg",
-    },
-    {
-      original: "/assets/26.jpg",
-      thumbnail: "/assets/26.jpg",
-    },
-    {
-      original: "/assets/27.jpg",
-      thumbnail: "/assets/27.jpg",
-    },
-    {
-      original: "/assets/28.jpg",
-      thumbnail: "/assets/28.jpg",
-    },
-    {
-      original: "/assets/29.jpg",
-      thumbnail: "/assets/29.jpg",
-    },
-    {
-      original: "/assets/30.jpg",
-      thumbnail: "/assets/30.jpg",
-    },
-    {
-      original: "/assets/31.jpg",
-      thumbnail: "/assets/31.jpg",
-    },
-    {
-      original: "/assets/32.jpg",
-      thumbnail: "/assets/32.jpg",
-    },
-    {
-      original: "/assets/33.jpg",
-      thumbnail: "/assets/33.jpg",
-    },
-    {
-      original: "/assets/34.jpg",
-      thumbnail: "/assets/34.jpg",
-    },
-    {
-      original: "/assets/35.jpg",
-      thumbnail: "/assets/35.jpg",
-    },
-    {
-      original: "/assets/36.jpg",
-      thumbnail: "/assets/36.jpg",
-    },
-    {
-      original: "/assets/37.jpg",
-      thumbnail: "/assets/37.jpg",
-    },
-    {
-      original: "/assets/38.jpg",
-      thumbnail: "/assets/38.jpg",
-    },
-    {
-      original: "/assets/39.jpg",
-      thumbnail: "/assets/39.jpg",
-    },
-    {
-      original: "/assets/40.jpg",
-      thumbnail: "/assets/40.jpg",
-    },
-    {
-      original: "/assets/41.jpg",
-      thumbnail: "/assets/41.jpg",
-    },
-    {
-      original: "/assets/42.jpg",
-      thumbnail: "/assets/42.jpg",
-    },
-    {
-      original: "/assets/43.jpg",
-      thumbnail: "/assets/43.jpg",
-    },
-    {
-      original: "/assets/44.jpg",
-      thumbnail: "/assets/44.jpg",
-    },
-    {
-      original: "/assets/45.jpg",
-      thumbnail: "/assets/45.jpg",
-    },
-  ];
+  const [inputValue, setInputValue] = useState(''); // Initialize input value state
+  const [message, setMessage] = useState('');
+  const word = inputValue.toLowerCase();
+  const MemoIndiraGallary = React.memo(IndiraGallary)
+  const MemoBubaGallary = React.memo(BubaGallary)
+  const checkWord = () => {
+  if (word === 'ghanashyam') {
+    setMessage('स्वागत छ,घनश्याम न्यौपाने !');
+
+  } else if( word === 'indira') {
+    setMessage('स्वागत छ, इन्दिरा!');
+  }
+}
   return (
-    <div>
-    <Gallary items={images}/>
+    <div className="container">
+          <div className="centered">
+      <input
+        type="text"
+        placeholder="एक गोप्य शब्द प्रवेश गर्नुहोस्।"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={()=>checkWord()}>click</button>
+      <p>{message&&message}</p>
+    </div>
+    {(word === 'indira')&&<MemoIndiraGallary />}
+    {word === 'ghanashyam'&&<MemoBubaGallary />}
     </div>
   );
 }
---
+++ src/components/gallery/didi/index.js
@@ -0,0 +1,186 @@
+import React from "react";
+import ImageGallery from "react-image-gallery";
+
+const IndiraGallary=()=> {
+  const images = [
+    {
+      original: "/assets/1.jpg",
+      thumbnail: "/assets/1.jpg",
+    },
+    {
+      original: "/assets/2.jpg",
+      thumbnail: "/assets/2.jpg",
+    },
+    {
+      original: "/assets/5.jpg",
+      thumbnail: "/assets/5.jpg",
+    },
+    {
+      original: "/assets/6.jpg",
+      thumbnail: "/assets/6.jpg",
+    },
+    {
+      original: "/assets/7.jpg",
+      thumbnail: "/assets/7.jpg",
+    },
+    {
+      original: "/assets/8.jpg",
+      thumbnail: "/assets/8.jpg",
+    },
+    {
+      original: "/assets/9.jpg",
+      thumbnail: "/assets/9.jpg",
+    },
+    {
+      original: "/assets/10.jpg",
+      thumbnail: "/assets/10.jpg",
+    },
+    {
+      original: "/assets/11.jpg",
+      thumbnail: "/assets/11.jpg",
+    },
+    {
+      original: "/assets/12.jpg",
+      thumbnail: "/assets/12.jpg",
+    },
+    {
+      original: "/assets/13.jpg",
+      thumbnail: "/assets/13.jpg",
+    },
+    {
+      original: "/assets/14.jpg",
+      thumbnail: "/assets/14.jpg",
+    },
+    {
+      original: "/assets/15.jpg",
+      thumbnail: "/assets/15.jpg",
+    },
+    {
+      original: "/assets/16.jpg",
+      thumbnail: "/assets/16.jpg",
+    },
+    {
+      original: "/assets/17.jpg",
+      thumbnail: "/assets/17.jpg",
+    },
+    {
+      original: "/assets/18.jpg",
+      thumbnail: "/assets/18.jpg",
+    },
+    {
+      original: "/assets/19.jpg",
+      thumbnail: "/assets/19.jpg",
+    },
+    {
+      original: "/assets/20.jpg",
+      thumbnail: "/assets/20.jpg",
+    },
+    {
+      original: "/assets/21.jpg",
+      thumbnail: "/assets/21.jpg",
+    },
+    {
+      original: "/assets/22.jpg",
+      thumbnail: "/assets/22.jpg",
+    },
+    {
+      original: "/assets/23.jpg",
+      thumbnail: "/assets/23.jpg",
+    },
+    {
+      original: "/assets/24.jpg",
+      thumbnail: "/assets/24.jpg",
+    },
+    {
+      original: "/assets/25.jpg",
+      thumbnail: "/assets/25.jpg",
+    },
+    {
+      original: "/assets/26.jpg",
+      thumbnail: "/assets/26.jpg",
+    },
+    {
+      original: "/assets/27.jpg",
+      thumbnail: "/assets/27.jpg",
+    },
+    {
+      original: "/assets/28.jpg",
+      thumbnail: "/assets/28.jpg",
+    },
+    {
+      original: "/assets/29.jpg",
+      thumbnail: "/assets/29.jpg",
+    },
+    {
+      original: "/assets/30.jpg",
+      thumbnail: "/assets/30.jpg",
+    },
+    {
+      original: "/assets/31.jpg",
+      thumbnail: "/assets/31.jpg",
+    },
+    {
+      original: "/assets/32.jpg",
+      thumbnail: "/assets/32.jpg",
+    },
+    {
+      original: "/assets/33.jpg",
+      thumbnail: "/assets/33.jpg",
+    },
+    {
+      original: "/assets/34.jpg",
+      thumbnail: "/assets/34.jpg",
+    },
+    {
+      original: "/assets/35.jpg",
+      thumbnail: "/assets/35.jpg",
+    },
+    {
+      original: "/assets/36.jpg",
+      thumbnail: "/assets/36.jpg",
+    },
+    {
+      original: "/assets/37.jpg",
+      thumbnail: "/assets/37.jpg",
+    },
+    {
+      original: "/assets/38.jpg",
+      thumbnail: "/assets/38.jpg",
+    },
+    {
+      original: "/assets/39.jpg",
+      thumbnail: "/assets/39.jpg",
+    },
+    {
+      original: "/assets/40.jpg",
+      thumbnail: "/assets/40.jpg",
+    },
+    {
+      original: "/assets/41.jpg",
+      thumbnail: "/assets/41.jpg",
+    },
+    {
+      original: "/assets/42.jpg",
+      thumbnail: "/assets/42.jpg",
+    },
+    {
+      original: "/assets/43.jpg",
+      thumbnail: "/assets/43.jpg",
+    },
+    {
+      original: "/assets/44.jpg",
+      thumbnail: "/assets/44.jpg",
+    },
+    {
+      original: "/assets/45.jpg",
+      thumbnail: "/assets/45.jpg",
+    },
+  ];
+  return (
+    <div>
+    <ImageGallery items={images}/>
+    </div>
+  );
+}
+
+export default IndiraGallary;
